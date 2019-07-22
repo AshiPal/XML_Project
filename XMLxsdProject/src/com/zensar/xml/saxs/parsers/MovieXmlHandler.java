d41e239d630d381a6e84f7da19b98ddef081027b
Ashi Pal <ashipal8@gmail.com>
2019-07-22 18:01:04 +0530

    added MovieXmlHandler

---
+++ XMLxsdProject/src/com/zensar/xml/saxs/parsers/MovieXmlHandler.java
@@ -0,0 +1,39 @@
+package com.zensar.xml.saxs.parsers;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MovieXmlHandler extends DefaultHandler {
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		for(int i=start;i<start+length;i++)
+			System.out.println(ch[i]);
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.endDocument();
+	}
+
+	@Override
+	public void endElement(String arg0, String arg1, String arg2) throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(arg0, arg1, arg2);
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.startDocument();
+	}
+
+	@Override
+	public void startElement(String arg0, String arg1, String arg2, Attributes arg3) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(arg0, arg1, arg2, arg3);
+	}
+
+}
